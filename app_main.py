21855cb29a32108266191d09b45e45a1279d7d6f
marvinxu99 <marvinxu99@hotmail.com>
2021-12-17 15:39:50 -0800

    added open tabs

--- app_main.py
+++ app_main.py
@@ -3,17 +3,51 @@ import typing
 from PyQt6.QtCore import *
 from PyQt6.QtGui import *
 from PyQt6.QtWidgets import *
-from PyQt6.uic.load_ui import loadUi
+from PyQt6.uic.load_ui import loadUi, loadUiType
 
+import MySQLdb
 
 
 class MainApp(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        loadUi("library.ui", self)
         self.setWindowTitle("Library Management System")
-        ui = loadUi("library.ui", self)
-        
+
+        self.handle_UI_changes()
+        self.handle_buttons()
+
+    def handle_UI_changes(self):
+        self.show_themes(False)
+
+    def handle_buttons(self):
+        self.btn_daily_operations.clicked.connect(self.open_daily_operations_tab)
+        self.btn_books.clicked.connect(self.open_books_tab)
+        self.btn_users.clicked.connect(self.open_users_tab)
+        self.btn_settings.clicked.connect(self.open_settings_tab)
+
+        self.btn_show_themes.clicked.connect(lambda: self.show_themes(True))
+        self.btn_hide_themes.clicked.connect(lambda: self.show_themes(False))
+
+    def show_themes(self, show_flag=True):
+        if show_flag:
+            self.themes_box.show()
+        else:
+            self.themes_box.hide()
+
+    def open_daily_operations_tab(self):
+        print("daily")
+        self.main_tab_widget.setCurrentIndex(0)
+
+    def open_books_tab(self):
+        self.main_tab_widget.setCurrentIndex(1)
+
+    def open_users_tab(self):
+        self.main_tab_widget.setCurrentIndex(2)
+
+    def open_settings_tab(self):
+        self.main_tab_widget.setCurrentIndex(3)
 
 
 def app_main():
