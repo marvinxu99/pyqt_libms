3f44aea40de576fa9b2948a57a76a94baac25451
marvinxu99 <marvinxu99@hotmail.com>
2021-12-17 18:13:41 -0800

    connected signals to slots

--- app_main.py
+++ app_main.py
@@ -20,15 +20,28 @@ class MainApp(QMainWindow):
 
     def handle_UI_changes(self):
         self.show_themes(False)
+        self.main_tab_widget.tabBar().setVisible(False)
 
     def handle_buttons(self):
+
+        # Main tabs
         self.btn_daily_operations.clicked.connect(self.open_daily_operations_tab)
         self.btn_books.clicked.connect(self.open_books_tab)
         self.btn_users.clicked.connect(self.open_users_tab)
         self.btn_settings.clicked.connect(self.open_settings_tab)
 
+        # Themes
         self.btn_show_themes.clicked.connect(lambda: self.show_themes(True))
         self.btn_hide_themes.clicked.connect(lambda: self.show_themes(False))
+
+        # Books
+        self.btn_add_new_book.clicked.connect(self.add_new_book)
+
+        # Settings
+        self.btn_add_category.clicked.connect(self.add_category)
+        self.btn_add_author.clicked.connect(self.add_author)
+        self.btn_add_publisher.clicked.connect(self.add_publisher)
+
 
     def show_themes(self, show_flag=True):
         if show_flag:
@@ -36,8 +49,10 @@ class MainApp(QMainWindow):
         else:
             self.themes_box.hide()
 
+    ###############################################
+    ## Open Tabs
+    ###############################################
     def open_daily_operations_tab(self):
-        print("daily")
         self.main_tab_widget.setCurrentIndex(0)
 
     def open_books_tab(self):
@@ -49,6 +64,82 @@ class MainApp(QMainWindow):
     def open_settings_tab(self):
         self.main_tab_widget.setCurrentIndex(3)
 
+
+    ###############################################
+    ## Books
+    ###############################################
+    def add_new_book(self):
+
+        self.db = MySQLdb.connect(host='localhost', user='winter', password="winter", db='library')
+        self.cur = self.db.cursor()
+
+        title       = self.new_book_title.text()
+        description = self.new_book_description.toPlainText()
+        code        = self.new_book_code.text()
+        category    = self.new_book_category.currentText()
+        author      = self.new_book_author.currentText()
+        publisher   = self.new_book_publisher.currentText()
+        price       = self.new_book_price.text()
+
+
+    def search_book(self):
+        pass
+
+
+    def edit_book(self):
+        pass
+
+    def delete_book(self):
+        pass
+
+
+    ###############################################
+    ## Users
+    ###############################################
+    def add_new_user(self):
+        pass
+
+
+    def user_login(self):
+        pass
+
+
+    def edit_user_data(self):
+        pass
+
+
+    ###############################################
+    ## Settings
+    ###############################################
+    def add_category(self):
+        self.db = MySQLdb.connect(host='localhost', user='winter', password="winter", db='library')
+        self.cur = self.db.cursor()
+
+        category_name = self.new_category_name.text()
+        self.cur.execute("INSERT INTO category (name) VALUES (%s)", (category_name,))
+        self.db.commit()
+        
+        self.statusBar().showMessage(f"New categoty: {category_name} added.")
+
+    def add_author(self):
+        self.db = MySQLdb.connect(host='localhost', user='winter', password="winter", db='library')
+        self.cur = self.db.cursor()
+
+        author_name = self.new_author_name.text()
+        self.cur.execute("INSERT INTO author (name) VALUES (%s)", (author_name,))
+        self.db.commit()
+        
+        self.statusBar().showMessage(f"New author: {author_name} added.")
+
+    def add_publisher(self):
+        self.db = MySQLdb.connect(host='localhost', user='winter', password="winter", db='library')
+        self.cur = self.db.cursor()
+
+        publisher_name = self.new_publisher_name.text()
+        self.cur.execute("INSERT INTO publisher (name) VALUES (%s)", (publisher_name,))
+        self.db.commit()
+        
+        self.statusBar().showMessage(f"New publisher: {publisher_name} added.")
 
 def app_main():
     app = QApplication(sys.argv)
